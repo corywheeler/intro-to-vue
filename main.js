bcf2937d4869053382a0d6bb0be89a507660c288
Cory Wheeler <corywheeler@gmail.com>
2020-08-25 11:03:48 -0400

    List Rendering

--- main.js
+++ main.js
@@ -2,11 +2,23 @@ var app = new Vue({
     el: '#app',
     data: {
         description: 'Silly White Whale\'s Playing',
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
         image: './assets/vmSocks-green.jpg',
         inStock: false,
         inventory: 8,
         link: 'https://quasimatic.com',
         onSale: true,
-        product: 'Socks'
+        product: 'Socks',
+        sizes: ["Small", "Medium", "Large"],
+        variants: [
+            {
+                variantId: 12345,
+                variantColor: 'green'
+            },
+            {
+                variantId: 67890,
+                variantColor: 'blue'
+            }
+        ]
     }
 });
