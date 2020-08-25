24bd9fce624e25819e2c402582671eaba18808d0
Cory Wheeler <corywheeler@gmail.com>
2020-08-25 12:11:16 -0400

    Event Handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
+        cart: 0,
         description: 'Silly White Whale\'s Playing',
         details: ["80% cotton", "20% polyester", "Gender-neutral"],
         image: './assets/vmSocks-green.jpg',
@@ -9,16 +10,30 @@ var app = new Vue({
         link: 'https://quasimatic.com',
         onSale: true,
         product: 'Socks',
-        sizes: ["Small", "Medium", "Large"],
+        sizes: ['Small', 'Medium', 'Large'],
         variants: [
             {
                 variantId: 12345,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: './assets/vmSocks-green.jpg'
             },
             {
                 variantId: 67890,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: './assets/vmSocks-blue.jpg'
             }
         ]
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if(this.cart > 0)
+                this.cart -= 1;
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage;
+        }
     }
 });
