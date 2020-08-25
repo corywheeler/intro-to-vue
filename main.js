513de24c069adf611a07e7d46a284ae703fff727
Cory Wheeler <corywheeler@gmail.com>
2020-08-25 09:43:07 -0400

    Attribute Binding

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'Silly White Whale\'s Playing'
+        description: 'Silly White Whale\'s Playing',
+        image: './assets/vmSocks-green.jpg',
+        link: 'https://quasimatic.com'
     }
 });
